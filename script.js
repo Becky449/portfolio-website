a59933ff790828f797a7019974aa797d53c91acc
Becky449 <82038825+Becky449@users.noreply.github.com>
2022-09-13 16:28:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const navItems = document.querySelector('.nav-items');
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navItems.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navItems.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-link").forEach( n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navItems.classList.remove("active");
-}))
-
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navItems.classList.remove("active");
+  }));
